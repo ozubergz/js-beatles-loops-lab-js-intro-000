fbb530e65cffc66e933a7117c3de4ebeab9cd861
James Min <jmin499@gmail.com>
2020-09-30 04:42:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 const theBeatlesPlay = (musicians, instruments) => {
-  
+  let arr = [];
+  for(let i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr;
 }
